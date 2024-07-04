efc074bf2edd8b040b505e53da91f21043e9b3c7
anshgrover21 <anshgrover25072002@gmail.com>
2024-07-04 11:30:12 +0530

    changes header name as per Given Sheet

--- SheetByChargeType/src/main/java/com/example/SheetByChargeType/Configuration/CommonConfig.java
+++ SheetByChargeType/src/main/java/com/example/SheetByChargeType/Configuration/CommonConfig.java
@@ -30,15 +30,14 @@ public class CommonConfig {
         flattenNameByProperty.put("transactionTime.", new StaticMapProperties(true, "Transaction Time"));
         flattenNameByProperty.put("transactionType.", new StaticMapProperties(true, "Transaction Type"));
         flattenNameByProperty.put("transactionReference.", new StaticMapProperties(true, "Transaction Reference"));
-        flattenNameByProperty.put("chargeSubType.", new StaticMapProperties(true, "Charge Sub Type"));
+        flattenNameByProperty.put("chargeSubType.", new StaticMapProperties(true, "Service Type"));
         flattenNameByProperty.put("courierAwbNo.", new StaticMapProperties(true, "Courier Awb No"));
-        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Details - Charge Type"));
+        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Type"));
         flattenNameByProperty.put("billingPeriodId.", new StaticMapProperties(true, "Billing Period Id"));
-        flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Total Charges - Currency Code"));
-        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges - Amount"));
+        flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Total Charges"));
+        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges"));
         flattenNameByProperty.put("gstPercentage.", new StaticMapProperties(true, "Gst Percentage"));
-        flattenNameByProperty.put("totalChargesWithGst.currencyCode.", new StaticMapProperties(true, "Total Charges With Gst - Currency Code"));
-        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges With Gst - Amount"));
+        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges (With Gst)"));
         return flattenNameByProperty;
     }
 
@@ -49,34 +48,34 @@ public class CommonConfig {
         flattenNameByProperty.put("transactionTime.", new StaticMapProperties(true, "Transaction Time"));
         flattenNameByProperty.put("transactionType.", new StaticMapProperties(true, "Transaction Type"));
         flattenNameByProperty.put("transactionReference.", new StaticMapProperties(true, "Transaction Reference"));
-        flattenNameByProperty.put("chargeSubType.", new StaticMapProperties(true, "Charge Sub Type"));
-        flattenNameByProperty.put("courierAwbNo.", new StaticMapProperties(true, "Courier Awb No"));
-        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Details - Charge Type"));
-        flattenNameByProperty.put("chargeDetails.shipmentValue.currencyCode.", new StaticMapProperties(true, "Charge Details - Shipment Value - Currency Code"));
-        flattenNameByProperty.put("chargeDetails.shipmentValue.amount.", new StaticMapProperties(true, "Charge Details - Shipment Value - Amount"));
-        flattenNameByProperty.put("chargeDetails.shipmentCreationDate.", new StaticMapProperties(true, "Charge Details - Shipment Creation Date"));
-        flattenNameByProperty.put("chargeDetails.sourcePincode.", new StaticMapProperties(true, "Charge Details - Source Pincode"));
-        flattenNameByProperty.put("chargeDetails.sourceCity.", new StaticMapProperties(true, "Charge Details - Source City"));
-        flattenNameByProperty.put("chargeDetails.destinationPincode.", new StaticMapProperties(true, "Charge Details - Destination Pincode"));
-        flattenNameByProperty.put("chargeDetails.destinationCity.", new StaticMapProperties(true, "Charge Details - Destination City"));
-        flattenNameByProperty.put("chargeDetails.zone.", new StaticMapProperties(true, "Charge Details - Zone"));
-        flattenNameByProperty.put("chargeDetails.courierCode.", new StaticMapProperties(true, "Charge Details - Courier Code"));
-        flattenNameByProperty.put("chargeDetails.deadWeight.", new StaticMapProperties(true, "Charge Details - Dead Weight"));
-        flattenNameByProperty.put("chargeDetails.volumeWeight.", new StaticMapProperties(true, "Charge Details - Volume Weight"));
-        flattenNameByProperty.put("chargeDetails.chargedWeight.", new StaticMapProperties(true, "Charge Details - Charged Weight"));
-        flattenNameByProperty.put("chargeDetails.weightSlab.", new StaticMapProperties(true, "Charge Details - Weight Slab"));
-        flattenNameByProperty.put("chargeDetails.shipmentType.", new StaticMapProperties(true, "Charge Details - Shipment Type"));
-        flattenNameByProperty.put("chargeDetails.paymentMode.", new StaticMapProperties(true, "Charge Details - Payment Mode"));
-        flattenNameByProperty.put("chargeDetails.freightCharges.currencyCode.", new StaticMapProperties(true, "Charge Details - Freight Charges - Currency Code"));
-        flattenNameByProperty.put("chargeDetails.freightCharges.amount.", new StaticMapProperties(true, "Charge Details - Freight Charges - Amount"));
-        flattenNameByProperty.put("chargeDetails.codCharges.currencyCode.", new StaticMapProperties(true, "Charge Details - Cod Charges - Currency Code"));
-        flattenNameByProperty.put("chargeDetails.codCharges.amount.", new StaticMapProperties(true, "Charge Details - Cod Charges - Amount"));
+        flattenNameByProperty.put("chargeSubType.", new StaticMapProperties(true, "Service Type"));
+        flattenNameByProperty.put("courierAwbNo.", new StaticMapProperties(true, "Courier Awb"));
+        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Type"));
+        flattenNameByProperty.put("chargeDetails.shipmentValue.currencyCode.", new StaticMapProperties(true, "Currency Code"));
+        flattenNameByProperty.put("chargeDetails.shipmentValue.amount.", new StaticMapProperties(true, "Shipment Value Amount"));
+        flattenNameByProperty.put("chargeDetails.shipmentCreationDate.", new StaticMapProperties(true, "Shipment Creation Date"));
+        flattenNameByProperty.put("chargeDetails.sourcePincode.", new StaticMapProperties(true, "Source Pincode"));
+        flattenNameByProperty.put("chargeDetails.sourceCity.", new StaticMapProperties(true, "Source City"));
+        flattenNameByProperty.put("chargeDetails.destinationPincode.", new StaticMapProperties(true, "Destination Pincode"));
+        flattenNameByProperty.put("chargeDetails.destinationCity.", new StaticMapProperties(true, "Destination City"));
+        flattenNameByProperty.put("chargeDetails.zone.", new StaticMapProperties(true, "Zone"));
+        flattenNameByProperty.put("chargeDetails.courierCode.", new StaticMapProperties(true, "Courier Code"));
+        flattenNameByProperty.put("chargeDetails.deadWeight.", new StaticMapProperties(true, "Dead Weight"));
+        flattenNameByProperty.put("chargeDetails.volumeWeight.", new StaticMapProperties(true, "Volume Weight"));
+        flattenNameByProperty.put("chargeDetails.chargedWeight.", new StaticMapProperties(true, "Charged Weight"));
+        flattenNameByProperty.put("chargeDetails.weightSlab.", new StaticMapProperties(true, "Weight Slab"));
+        flattenNameByProperty.put("chargeDetails.shipmentType.", new StaticMapProperties(true, "Shipment Type"));
+        flattenNameByProperty.put("chargeDetails.paymentMode.", new StaticMapProperties(true, "Payment Mode"));
+        flattenNameByProperty.put("chargeDetails.freightCharges.currencyCode.", new StaticMapProperties(true, "Currency Code"));
+        flattenNameByProperty.put("chargeDetails.freightCharges.amount.", new StaticMapProperties(true, "Freight Charges Amount"));
+        flattenNameByProperty.put("chargeDetails.codCharges.currencyCode.", new StaticMapProperties(true, "Currency Code"));
+        flattenNameByProperty.put("chargeDetails.codCharges.amount.", new StaticMapProperties(true, "Cod Charges Amount"));
         flattenNameByProperty.put("billingPeriodId.", new StaticMapProperties(true, "Billing Period Id"));
-        flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Total Charges - Currency Code"));
-        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges - Amount"));
+        flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Currency Code"));
+        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges Amount"));
         flattenNameByProperty.put("gstPercentage.", new StaticMapProperties(true, "Gst Percentage"));
-        flattenNameByProperty.put("totalChargesWithGst.currencyCode.", new StaticMapProperties(true, "Total Charges With Gst - Currency Code"));
-        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges With Gst - Amount"));
+        flattenNameByProperty.put("totalChargesWithGst.currencyCode.", new StaticMapProperties(true, "Currency Code"));
+        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges ( With Gst )"));
         return flattenNameByProperty;
     }
     @Bean
@@ -86,15 +85,15 @@ public class CommonConfig {
         flattenNameByProperty.put("transactionTime.", new StaticMapProperties(true, "Transaction Time"));
         flattenNameByProperty.put("transactionType.", new StaticMapProperties(true, "Transaction Type"));
         flattenNameByProperty.put("transactionReference.", new StaticMapProperties(true, "Transaction Reference"));
-        flattenNameByProperty.put("chargeSubType.", new StaticMapProperties(true, "Charge Sub Type"));
-        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Details - Charge Type"));
-        flattenNameByProperty.put("chargeDetails.activationDate.", new StaticMapProperties(true, "Charge Details - Activation Date"));
-        flattenNameByProperty.put("chargeDetails.endDate.", new StaticMapProperties(true, "Charge Details - End Date"));
+        flattenNameByProperty.put("chargeSubType.", new StaticMapProperties(true, "Service Type"));
+        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Type"));
+        flattenNameByProperty.put("chargeDetails.activationDate.", new StaticMapProperties(true, "Activation Date"));
+        flattenNameByProperty.put("chargeDetails.endDate.", new StaticMapProperties(true, "End Date"));
         flattenNameByProperty.put("billingPeriodId.", new StaticMapProperties(true, "Billing Period Id"));
-        flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Total Charges - Currency Code"));
-        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges - Amount"));
-        flattenNameByProperty.put("totalChargesWithGst.currencyCode.", new StaticMapProperties(true, "Total Charges With Gst - Currency Code"));
-        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges With Gst - Amount"));
+        flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Currency Code"));
+        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges"));
+        flattenNameByProperty.put("gstPercentage.", new StaticMapProperties(true, "Gst Percentage"));
+        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges (With Gst)"));
         return flattenNameByProperty;
     }
     @Bean
@@ -104,15 +103,14 @@ public class CommonConfig {
         flattenNameByProperty.put("transactionTime.", new StaticMapProperties(true, "Transaction Time"));
         flattenNameByProperty.put("transactionType.", new StaticMapProperties(true, "Transaction Type"));
         flattenNameByProperty.put("transactionReference.", new StaticMapProperties(true, "Transaction Reference"));
-        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Details - Charge Type"));
-        flattenNameByProperty.put("chargeDetails.platformName.", new StaticMapProperties(true, "Charge Details - Platform Name"));
-        flattenNameByProperty.put("chargeDetails.liveDate.", new StaticMapProperties(true, "Charge Details - Live Date"));
+        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Type"));
+        flattenNameByProperty.put("chargeDetails.platformName.", new StaticMapProperties(true, "Platform Name"));
+        flattenNameByProperty.put("chargeDetails.liveDate.", new StaticMapProperties(true, "Live Date"));
         flattenNameByProperty.put("billingPeriodId.", new StaticMapProperties(true, "Billing Period Id"));
         flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Total Charges - Currency Code"));
-        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges - Amount"));
+        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges"));
         flattenNameByProperty.put("gstPercentage.", new StaticMapProperties(true, "Gst Percentage"));
-        flattenNameByProperty.put("totalChargesWithGst.currencyCode.", new StaticMapProperties(true, "Total Charges (With Gst) - Currency Code"));
-        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges (With Gst) - Amount"));
+        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges (With Gst)"));
         return flattenNameByProperty;
     }
 
@@ -123,16 +121,13 @@ public class CommonConfig {
         flattenNameByProperty.put("transactionTime.", new StaticMapProperties(true, "Transaction Time"));
         flattenNameByProperty.put("transactionType.", new StaticMapProperties(true, "Transaction Type"));
         flattenNameByProperty.put("transactionReference.", new StaticMapProperties(true, "Transaction Reference"));
-        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Details - Charge Type"));
-        flattenNameByProperty.put("chargeDetails.orderValue.currencyCode.", new StaticMapProperties(true, "Charge Details - Order Value - Currency Code"));
-        flattenNameByProperty.put("chargeDetails.orderValue.amount.", new StaticMapProperties(true, "Charge Details - Order Value - Amount"));
-        flattenNameByProperty.put("chargeDetails.percentageFee.", new StaticMapProperties(true, "Charge Details - Percentage Fee"));
+        flattenNameByProperty.put("chargeDetails.chargeType.", new StaticMapProperties(true, "Charge Type"));
+        flattenNameByProperty.put("chargeDetails.orderValue.amount.", new StaticMapProperties(true, "Charge Details Amount"));
+        flattenNameByProperty.put("chargeDetails.percentageFee.", new StaticMapProperties(true, "Percentage Fee"));
         flattenNameByProperty.put("billingPeriodId.", new StaticMapProperties(true, "Billing Period Id"));
-        flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Total Charges - Currency Code"));
-        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges - Amount"));
-        flattenNameByProperty.put("totalChargesWithGst.currencyCode.", new StaticMapProperties(true, "Total Charges With Gst - Currency Code"));
-        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges With Gst - Amount"));
-
+        flattenNameByProperty.put("totalCharges.currencyCode.", new StaticMapProperties(true, "Total Charges Currency Code"));
+        flattenNameByProperty.put("totalCharges.amount.", new StaticMapProperties(true, "Total Charges Amount"));
+        flattenNameByProperty.put("totalChargesWithGst.amount.", new StaticMapProperties(true, "Total Charges (With Gst)"));
         return flattenNameByProperty;
     }
 
